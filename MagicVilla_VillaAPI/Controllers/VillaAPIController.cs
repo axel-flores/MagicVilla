b8c1383c227f26711c11885ec6e8e888e5dc89b6
axel-flores <axel.flores.tonacatl@gmail.com>
2023-04-15 21:56:46 -0600

    Updated: primeros pasos

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa {Id = 1, Name = "villa pro"},
+                new Villa { Id = 2, Name = "villa junior" }
+            };
+        }
+    }
+}
