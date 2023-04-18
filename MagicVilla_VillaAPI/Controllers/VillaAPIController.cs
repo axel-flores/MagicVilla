7b53f49984dc7a3c5ba985b889e1dac1bd6be612
AxelFT <axelpsb@gmail.com>
2023-04-17 23:24:19 -0600

    Updated: agregados metodos post

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,6 @@
-﻿using MagicVilla_VillaAPI.Models;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
@@ -7,18 +9,69 @@ namespace MagicVilla_VillaAPI.Controllers
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return new List<Villa>
+            return Ok(VillaStore.villaList);
+        }
+
+        /*
+         * ActionResult<VillaDTO> especifica el tipo de objeto que va a regresar
+         * otra forma es poner el tipo en ProducesResponseType, ejemplo:
+         * [ProducesResponseType(200, Type = typeof(VillaDTO))]
+         */
+        [HttpGet("{id:int}", Name="GetVilla")]
+        //[ProducesResponseType(200)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0) { return BadRequest(); }
+
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            if (villa == null) { NotFound(); }
+
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDTO> PostVilla([FromBody] VillaDTO villaDTO)
+        {
+            if(villaDTO == null)
             {
-                new Villa {Id = 1, Name = "villa pro"},
-                new Villa { Id = 2, Name = "villa junior" }
-            };
+                return BadRequest();
+            }
+
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError );
+            }
+
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villaDTO);
+
+            //var villaBd = VillaStore.villaList.FirstOrDefault( v => v.Id == villaDTO.Id);
+
+            //if (villaBd == null) {
+            //    villaBd = new VillaDTO(); 
+            //    villaBd.Id = villaDTO.Id;
+            //    villaBd.Name = villaDTO.Name;
+            
+            //    VillaStore.villaList.Add(villaBd);
+            //}
+            
+            //return Ok(villaDTO);
+
+            //esto devuelve la ruta en la que se creo, invoca el metodo getvilla
+            return CreatedAtRoute("GetVilla", new {id = villaDTO.Id}, villaDTO);
+            //CreatedAtRoute devuelve el codigo 201
         }
     }
 }
