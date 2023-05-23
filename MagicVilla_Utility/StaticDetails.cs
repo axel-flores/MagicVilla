f2fd60cc0e2322db318f9165946537c1068eb5a0
Axel Flores <axelpsb@gmail.com>
2023-05-23 15:28:27 -0600

    Added: models agregados al proyecto web

---
+++ MagicVilla_Utility/StaticDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace MagicVilla_Utility
+{
+    public static class StaticDetails
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT, 
+            DELETE
+        }
+
+    }
+}
